5e617d1465450db7e44c1ac07b150fdcc862c7cc
JavierLlanos6 <xavi6poop@gmail.com>
2022-11-27 19:34:33 -0400

    added css to the form

--- src/components/EditarCapitulo/Content/Class/Class.jsx
+++ src/components/EditarCapitulo/Content/Class/Class.jsx
@@ -14,7 +14,9 @@ export const Class = () => {
     <div className={styles.Class}>
       <h1>Editar Capítulo</h1>
       <form>
-        <label htmlFor="titulocap">Nombre del capitulo: </label>
+        <label htmlFor="titulocap" className={styles.textform}>
+          Nombre del capitulo:{" "}
+        </label>
         <input
           type="text"
           id="titulocap"
@@ -22,11 +24,14 @@ export const Class = () => {
           placeholder="Ingrese nombre del capitulo"
           value={titulocap}
           onChange={(e) => setTitulocap(e.target.value)}
+          className={styles.box}
         />
 
         <br />
         <br />
-        <label htmlFor="EnlaceMat">Archivos: </label>
+        <label htmlFor="EnlaceMat" className={styles.textform}>
+          Archivos:{" "}
+        </label>
         <input
           type="text"
           id="EnlaceMat"
@@ -34,23 +39,29 @@ export const Class = () => {
           placeholder="Ingrese el enlace del archivo"
           value={EnlaceMat}
           onChange={(e) => setEnlaceMat(e.target.value)}
+          className={styles.box}
         />
 
         <br />
         <br />
-        <label htmlFor="EstadoForo">Estado del foro: </label>
+        <label htmlFor="EstadoForo" className={styles.textform}>
+          Estado del foro:{" "}
+        </label>
         <select
           name="EstadoForo"
           onChange={(e) => setEstadoForo(e.target.value)}
         >
           <option value="Inactivo">Inactivo</option>
           <option value="Activo">Activo</option>
+          className={styles.box}
         </select>
         <br />
         <br />
-        <h3>Ejemplo practico</h3>
+        <p className={styles.texth3}>Ejemplo practico</p>
         <br />
-        <label htmlFor="TituloEjem">Titulo del ejemplo: </label>
+        <label htmlFor="TituloEjem" className={styles.textform}>
+          Titulo del ejemplo:{" "}
+        </label>
         <input
           type="text"
           id="TituloEjem"
@@ -58,33 +69,40 @@ export const Class = () => {
           placeholder="Ingrese el titulo del ejemplo practico"
           value={TituloEjem}
           onChange={(e) => setTituloEjem(e.target.value)}
+          className={styles.box}
         />
 
         <br />
         <br />
-        <label htmlFor="DescripEjem">Descripcion del ejemplo:</label>
+        <label htmlFor="DescripEjem" className={styles.textform}>
+          Descripcion del ejemplo:
+        </label>
         <input
           type="text"
           id="DescripEjem"
           name="DescripEjem"
           value={DescripEjem}
           onChange={(e) => setDescripEjem(e.target.value)}
+          className={styles.box}
         />
 
         <br />
         <br />
-        <label htmlFor="CodigoEjem">Codigo del ejemplo</label>
+        <label htmlFor="CodigoEjem" className={styles.textform}>
+          Codigo del ejemplo:{" "}
+        </label>
         <input
           type="text"
           id="CodigoEjem"
           name="CodigoEjem"
           value={CodigoEjem}
           onChange={(e) => setCodigoEjem(e.target.value)}
+          className={styles.box}
         />
       </form>
       <br />
       <br />
-      <button id="botonNuevoCap" type="button">
+      <button id="botonNuevoCap" type="button" className={styles.button}>
         Publicar capitulo{" "}
       </button>
     </div>
--- src/components/NuevoCap/Content/Class/Class.jsx
+++ src/components/NuevoCap/Content/Class/Class.jsx
@@ -18,19 +18,25 @@ export const Class = () => {
     <div className={styles.Class}>
       <h1>Nuevo Capítulo</h1>
       <form>
-        <label htmlFor="titulocap">Titulo del capitulo: </label>
+        <label htmlFor="titulocap" className={styles.textform}>
+          Titulo del capitulo:{" "}
+        </label>
         <input
           type="text"
           id="titulocap"
           name="titulocap"
-          placeholder="Ingrese el nomnbre del capitulo"
+          class="box"
+          placeholder="Ingrese el nombre del capitulo"
           value={titulocap}
           onChange={(e) => setTitulocap(e.target.value)}
+          className={styles.box}
         />
 
         <br />
         <br />
-        <label htmlFor="titulomat">Titulo del material: </label>
+        <label htmlFor="titulomat" className={styles.textform}>
+          Titulo del material:{" "}
+        </label>
         <input
           type="text"
           id="titulomat"
@@ -38,22 +44,28 @@ export const Class = () => {
           placeholder="Ingrese el nombre del material"
           value={titulomat}
           onChange={(e) => setTitulomat(e.target.value)}
+          className={styles.box}
         />
 
         <br />
         <br />
-        <label htmlFor="DescripcionForo">Descripcion del foro: </label>
+        <label htmlFor="DescripcionForo" className={styles.textform}>
+          Descripcion del foro:{" "}
+        </label>
         <input
           type="text"
           id="DescripcionForo"
           name="DescripcionForo"
           value={DescripcionForo}
           onChange={(e) => setDescripcionForo(e.target.value)}
+          className={styles.box}
         />
 
         <br />
         <br />
-        <label htmlFor="EnlaceMat">Enlace del material de estudio </label>
+        <label htmlFor="EnlaceMat" className={styles.textform}>
+          Enlace del material de estudio:{" "}
+        </label>
         <input
           type="text"
           id="EnlaceMat"
@@ -61,11 +73,14 @@ export const Class = () => {
           placeholder="Ingrese el enlace del archivo"
           value={EnlaceMat}
           onChange={(e) => setEnlaceMat(e.target.value)}
+          className={styles.box}
         />
 
         <br />
         <br />
-        <label htmlFor="tituloforo">Titulo del foro </label>
+        <label htmlFor="tituloforo" className={styles.textform}>
+          Titulo del foro:{" "}
+        </label>
         <input
           type="text"
           id="tituloforo"
@@ -73,34 +88,43 @@ export const Class = () => {
           placeholder="Ingrese el titulo del foro para este capitulo"
           value={tituloforo}
           onChange={(e) => setTituloforo(e.target.value)}
+          className={styles.box}
         />
 
         <br />
         <br />
-        <label htmlFor="DescripForo">Descripcion del foro</label>
+        <label htmlFor="DescripForo" className={styles.textform}>
+          Descripcion del foro:{" "}
+        </label>
         <input
           type="text"
           id="DescripForo"
           name="DescripForo"
           value={DescripForo}
           onChange={(e) => setDescripForo(e.target.value)}
+          className={styles.box}
         />
 
         <br />
         <br />
-        <label htmlFor="EstadoForo">Estado del foro: </label>
+        <label htmlFor="EstadoForo" className={styles.textform}>
+          Estado del foro:{" "}
+        </label>
         <select
           name="EstadoForo"
           onChange={(e) => setEstadoForo(e.target.value)}
         >
           <option value="Inactivo">Inactivo</option>
           <option value="Activo">Activo</option>
+          className={styles.box}
         </select>
         <br />
         <br />
-        <h3>Ejemplo practico</h3>
+        <p className={styles.texth3}>Ejemplo practico</p>
         <br />
-        <label htmlFor="TituloEjem">Titulo del ejemplo: </label>
+        <label htmlFor="TituloEjem" className={styles.textform}>
+          Titulo del ejemplo:{" "}
+        </label>
         <input
           type="text"
           id="TituloEjem"
@@ -108,33 +132,40 @@ export const Class = () => {
           placeholder="ingrese el titulo del ejemplo practico"
           value={TituloEjem}
           onChange={(e) => setTituloEjem(e.target.value)}
+          className={styles.box}
         />
 
         <br />
         <br />
-        <label htmlFor="DescripEjem">Descripcion del ejemplo:</label>
+        <label htmlFor="DescripEjem" className={styles.textform}>
+          Descripcion del ejemplo:
+        </label>
         <input
           type="text"
           id="DescripEjem"
           name="DescripEjem"
           value={DescripEjem}
           onChange={(e) => setDescripEjem(e.target.value)}
+          className={styles.box}
         />
 
         <br />
         <br />
-        <label htmlFor="CodigoEjem">Codigo del ejemplo</label>
+        <label htmlFor="CodigoEjem" className={styles.textform}>
+          Codigo del ejemplo:{" "}
+        </label>
         <input
           type="text"
           id="CodigoEjem"
           name="CodigoEjem"
           value={CodigoEjem}
           onChange={(e) => setCodigoEjem(e.target.value)}
+          className={styles.box}
         />
       </form>
       <br />
       <br />
-      <button id="botonNuevoCap" type="button">
+      <button id="botonNuevoCap" type="button" className={styles.button}>
         Publicar capitulo{" "}
       </button>
     </div>
